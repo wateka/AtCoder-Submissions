6574f89c2077ced79b3fb7654dda5d2bebc72659
AutoCloneBot <acautoclone@bot.com>
2024-03-31 14:50:48 +0000

    Added my codes from AtCoder

---
+++ abc347/abc347_b.rs
@@ -0,0 +1,31 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+
+use std::collections::{HashMap, HashSet, BTreeMap, BTreeSet};
+use ac_library::{dsu::Dsu, modint::ModInt998244353 as Mint9, modint::Mod1000000007 as Mint10};
+use itertools::{Itertools, izip, iproduct};
+use superslice::Ext;
+use proconio::{fastout, input, marker::{Chars, Usize1}};
+macro_rules! yes_no { ($condition: expr) => {println!("{}", if $condition {"Yes"} else {"No"})} }
+macro_rules! putln { ($value: expr) => {println!("{}", $value)} }
+
+#[fastout]
+fn main() {
+    input! {
+        s: Chars
+    }
+
+    let mut set = HashSet::new();
+
+    let len = s.len();
+    for i in 0..len {
+        for j in i..len {
+            let ss = &s[i..=j];
+            set.insert(ss);
+            // eprintln!("{:?}", ss);
+        }
+    }
+
+    putln!(set.len());
+}
