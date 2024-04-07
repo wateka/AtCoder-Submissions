b304be2e5cb2780a45a374bcf502f897d4ffce49
AutoCloneBot <acautoclone@bot.com>
2024-04-07 14:51:01 +0000

    Added my codes from AtCoder

---
+++ abc348/abc348_c.rs
@@ -0,0 +1,28 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(unused_macros)]
+
+use std::{collections::{BTreeMap, BTreeSet, HashMap, HashSet}, hash::Hash};
+use ac_library::{dsu::Dsu, modint::ModInt998244353 as Mint9, modint::Mod1000000007 as Mint10};
+use itertools::{Itertools, izip, iproduct};
+use superslice::Ext;
+use proconio::{fastout, input, marker::{Chars, Usize1}};
+macro_rules! yes_no { ($condition: expr) => {println!("{}", if $condition {"Yes"} else {"No"})} }
+macro_rules! putln { ($value: expr) => {println!("{}", $value)} }
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        ac: [(i64, usize); n],
+    }
+
+    let mut map = HashMap::new();
+    for (ai, ci) in ac {
+        let &v = map.get(&ci).unwrap_or(&i64::MAX).min(&ai);
+        map.insert(ci, v);
+    }
+
+    let ans = map.iter().map(|(_k, v)| v).max().unwrap();
+    putln!(ans);
+}
